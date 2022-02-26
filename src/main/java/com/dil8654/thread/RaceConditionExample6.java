c1a9e1448caf68f1b1b798e65ed0d70acc3008c1
DilankaM <dilanka85@gmail.com>
2022-02-26 23:06:51 +0530

    thread race condition examples

---
+++ src/main/java/com/dil8654/thread/RaceConditionExample6.java
@@ -0,0 +1,39 @@
+package com.dil8654.thread;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author DilankaM
+ * @created 26/02/2022 - 22:58
+ */
+public class RaceConditionExample6 {
+
+  public static void main(String[] args) {
+
+    Map<String, String> sharedResourceMap = new ConcurrentHashMap<>();
+
+    Thread thread1 = new Thread(getRunnable(sharedResourceMap));
+    Thread thread2 = new Thread(getRunnable(sharedResourceMap));
+
+    thread1.start();
+    thread2.start();
+  }
+
+  private static Runnable getRunnable(Map<String, String> sharedResourceMap) {
+    return () -> {
+      for (int i = 0; i < 1000000; i++) {
+        synchronized (sharedResourceMap) {
+          if (sharedResourceMap.containsKey("key")) {
+            String val = sharedResourceMap.remove("key");
+            if (val == null) {
+              System.out.println("iteration index + " + i + " , key is: " + val);
+            }
+          } else {
+            sharedResourceMap.put("key", "my value");
+          }
+        }
+      }
+    };
+  }
+}
